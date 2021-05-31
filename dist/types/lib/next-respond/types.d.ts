035720c57d388bf8dfb72da15c02f35be446534d
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-05-30 18:05:22 -0700

    refactor: checkpoint

--- dist/types/lib/next-respond/types.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type SuccessJsonResponse = Record<string, unknown> & {
-    success: true;
-};
-export declare type ErrorJsonResponse = Record<string, unknown> & {
-    error: string;
-    success: false;
-};
-export type { HttpStatusCode } from '@ergodark/types';
--- dist/types/src/backend/error.d.ts
+++
@@ -1,8 +0,0 @@
-import { AppError } from 'named-app-errors';
-export * from 'named-app-errors';
-export declare class ActivityGenerationError extends AppError {
-    constructor(message?: string);
-}
-export declare class IdTypeError<T = string | number> extends AppError {
-    constructor(id?: T);
-}
