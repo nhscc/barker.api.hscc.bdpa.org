9c91d972353217e58ee22fc3cbe667ad9ded1440
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-06-06 18:03:18 -0700

    refactor(error.ts): prepare for named-app-errors error type updates

--- src/backend/error.ts
+++ src/backend/error.ts
@@ -1,9 +1,31 @@
 import { AppError, KeyTypeError, makeNamedError } from 'named-app-errors';
 
-export * from 'named-app-errors';
+export {
+  AppError,
+  FetchError,
+  GuruMeditationError,
+  HookError,
+  KeyError,
+  NotAuthorizedError,
+  ValidationError,
+  KeyTypeError as InvalidKeyError,
+  NotFoundError as ItemNotFoundError,
+  makeNamedError
+} from 'named-app-errors';
 
-// TODO: update named-app-errors with new paradigm
-export { KeyTypeError as InvalidKeyError };
+// TODO: update named-app-errors with new naming paradigm:
+// TODO:   - rename "XTypeError"s
+// TODO:   - add InvalidIdError
+// TODO:   - BC: rename "NotFoundError" to "ItemNotFoundError" and
+// TODO:     "NotFoundError" now takes any message
+
+export class NotFoundError extends AppError {
+  constructor(message?: string) {
+    super(message || 'resource not found');
+  }
+}
+
+makeNamedError(NotFoundError, 'NotFoundError');
 
 export class ActivityGenerationError extends AppError {
   constructor(message?: string) {
