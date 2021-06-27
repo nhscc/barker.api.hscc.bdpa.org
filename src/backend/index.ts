320912ee3e7adc086ceaf31870558e71fb108df9
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-06-27 15:24:16 -0700

    fix(backend): use more permissive email regex

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -38,9 +38,7 @@ const isObject = (object: unknown) =>
   !Array.isArray(object) && object !== null && typeof object == 'object';
 
 const nameRegex = /^[a-zA-Z0-9 -]+$/;
-const emailRegex =
-  // eslint-disable-next-line no-control-regex
-  /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 const phoneRegex =
   /^(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?$/;
 const usernameRegex = /^[a-zA-Z0-9_-]{5,20}$/;
